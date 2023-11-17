5860b6963045a0795f491ea80cab6d24e3c01eaf
Naila <Naila@DESKTOP-JMQF5ST>
2023-11-17 11:15:47 +0000

    comments added

--- DeLP_GDPR Project/src/DeLP_GDPR/delp/reasoner/DelpReasoner.java
+++ DeLP_GDPR Project/src/DeLP_GDPR/delp/reasoner/DelpReasoner.java
@@ -30,6 +30,11 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 
 	/**
 	 * The comparison criterion is initialized with the "empty criterion"
+	 * 
+	 * comparisonCriterion: This variable holds the comparison criterion 
+	 * used for inference. It is initially set to an instance of EmptyCriterion
+	 *  (a default criterion) but can be customized through the constructor.
+	 * 
 	 */
 	private ComparisonCriterion comparisonCriterion = new EmptyCriterion();
 	
@@ -38,13 +43,16 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 	 * Creates a new DelpReasoner for the given delp.	 * 
 	 * @param comparisonCriterion a comparison criterion used for inference
 	 */
+	
+
+	/**The constructor initializes the reasoner with a specified comparison criterion.*/
 	public DelpReasoner(ComparisonCriterion comparisonCriterion) {
 		this.comparisonCriterion = comparisonCriterion;
 		
 	}
 
 	/**
-	 * returns the comparison criterion used in this program
+	 * This method returns the comparison criterion used in the reasoner.
 	 * @return the comparison criterion used in this program
 	 */
 	public ComparisonCriterion getComparisonCriterion() {
@@ -57,11 +65,23 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 	 * @param delp a delp
 	 * @return a set of <code>DelpArgument</code> that are warrants
 	 */
+	
+	/**This is the method signature. It declares a public method named 
+	 * getWarrants that takes a DefeasibleLogicProgram parameter delp and returns a Set of DelpArgument.*/
     public Set<DelpArgument> getWarrants(DefeasibleLogicProgram delp){
+    	// Ground the defeasible logic program
+    	/**It creates a new DefeasibleLogicProgram called groundDelp by grounding the input delp.*/
     	DefeasibleLogicProgram groundDelp = delp.ground();
+    	// Get all arguments from the grounded defeasible logic program
+    	/**It retrieves all arguments from the grounded defeasible logic program and stores them in a set called all_arguments.*/
         Set<DelpArgument> all_arguments = groundDelp.ground().getArguments();
+     // Use stream to filter and collect warranted arguments
+        /**It converts the set of arguments into a stream, which allows for functional-style processing of elements.*/
 		return all_arguments.stream()
+				/**It applies a filter operation to the stream, keeping only those arguments for which the isWarrant
+				 *  method returns true. The isWarrant method checks if an argument is a warrant.*/
                 .filter(argument -> isWarrant(groundDelp,argument))
+                //It collects the filtered arguments into a new Set using the toSet() collector.
                 .collect(Collectors.toSet());
 	}
 
@@ -73,13 +93,25 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 	 * @return <source>true</source> iff <source>argument</source> is a warrant given <source>arguments</source>.
 	 */
 	private boolean isWarrant(DefeasibleLogicProgram groundDelp, DelpArgument argument){
+		// Step 1: Create a new DialecticalTree with the given argument
+		//Create a new DialecticalTree object with the given argument.
 		DialecticalTree dtree = new DialecticalTree(argument);
+		// Step 2: Initialize a stack for tree traversal
+		// Initialize a stack (Deque) and add the initial DialecticalTree to it.
 		Deque<DialecticalTree> stack = new ArrayDeque<>();
 		stack.add(dtree);
+		// Step 3: Perform tree traversal until the stack is empty
+		/**Perform tree traversal using a while loop until the stack is empty.
+		Pop the top element (dtree2) from the stack.
+		Add all defeaters of the popped element to the stack.*/
 		while(!stack.isEmpty()){
+			// Step 4: Pop the top element from the stack
 			DialecticalTree dtree2 = stack.pop();
+			// Step 5: Add all defeaters of the popped element to the stack
 			stack.addAll(dtree2.getDefeaters(groundDelp,comparisonCriterion));
 		}
+		// Step 6: Check if the marking of the original DialecticalTree is UNDEFEATED
+		/** The method returns true if the marking is UNDEFEATED, indicating that the argument is a warrant, and false otherwise.*/
 		return dtree.getMarking().equals(DialecticalTree.Mark.UNDEFEATED);
 	}
 	
@@ -88,33 +120,52 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 	 * @param delp some delp.
 	 * @param f a formula
 	 * @return all arguments with the given conclusion from the delp.
+	 * 
+	 * The primary purpose of this method is to identify and return
+	 *  minimal arguments that, together with their associated rules, 
+	 *  collectively support a specified conclusion within a defeasible logic program
 	 */
 	public static Set<DelpArgument> getArgumentsWithConclusion(DefeasibleLogicProgram delp, FolFormula f){
+		// Step 1: Create a set to store all rules
+		/**Create a collection to store all defeasible rules (allRules).
+		Populate allRules with rules from the defeasible logic program (delp).
+		Generate all derivations (allDerivations) for the given formula (f).*/
 		Collection<DelpRule> allRules = new HashSet<DelpRule>();
 		allRules.addAll(delp);
+		// Step 2: Generate all derivations for the given formula
+		/**Create a set (arguments) to store DelpArgument objects.
+		Iterate through each derivation.*/
 		Set<Derivation<DelpRule>> allDerivations = Derivation.allDerivations(allRules, f);
+		// Step 3: Create a set to store DelpArguments
 		Set<DelpArgument> arguments = new HashSet<>();
+		// Step 4: Iterate through each derivation. Iterate through each derivation.
+		
 		for(Derivation<DelpRule> derivation: allDerivations){
+			// Step 5: Extract defeasible rules from the derivation
 			Set<DefeasibleRule> rules = derivation.stream()
                     .filter(rule -> rule instanceof DefeasibleRule)
                     .map(rule -> (DefeasibleRule) rule)
                     .collect(Collectors.toSet());
-            // consistency check: rules have to be consistent with strict knowledge part
+			// Step 6: Consistency check - rules must be consistent with the strict knowledge part
 			if(delp.isConsistent(rules))
+				// Step 7: Add a new DelpArgument to the set with the conclusion of the derivation
 				arguments.add(new DelpArgument(rules,(FolFormula)derivation.getConclusion()));
 		}
-		// subargument test
+		// Step 8: Subargument test - filter out non-minimal arguments
 		Set<DelpArgument> result = new HashSet<>();
 		for(DelpArgument argument1: arguments){
 			boolean is_minimal = true;
 			for(DelpArgument argument2: arguments){
+				// Step 9: Check if argument1 is a strong subargument of argument2
 				if(argument1.getConclusion().equals(argument2.getConclusion()) && argument2.isStrongSubargumentOf(argument1)){
 					is_minimal = false;
 					break;
 				}
 			}
+			// Step 10: If argument1 is minimal, add it to the result set
 			if(is_minimal) result.add(argument1);
 		}
+		// Step 11: Return the set of minimal arguments with the given conclusion
 		return result;
 	}
 
@@ -123,19 +174,25 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 	 */
 	@Override
 	public Type query(DefeasibleLogicProgram delp, FolFormula f) {
-		// check query:		
+		// check query:	
+		// Verifies that the formula (f) is a literal and is ground. If not, it throws an exception.
 		if(!f.isLiteral())
 			throw new IllegalArgumentException("Formula is expected to be a literal: "+f);
 		if(!f.isGround())
 			throw new IllegalArgumentException("Formula is expected to be ground: "+f);
-
+		// Ground the Defeasible Logic Program:
+		// Creates a grounded version of the defeasible logic program
 		DefeasibleLogicProgram groundDelp = delp.ground();
-		//int inferenceSteps = 0;
-		// get all arguments for f
+		// Initialize Variables:
+		//nitializes a boolean variable (warrant) to track whether there is a warrant for the formula.
+		//Retrieves all arguments that support the formula (f) using the getArgumentsWithConclusion method.
 		boolean warrant = false;		
 		Set<DelpArgument> args = getArgumentsWithConclusion(groundDelp, f);
+		// Process Arguments Supporting f:
+		// For each argument (arg) supporting the formula (f), creates a dialectical tree (dtree) rooted at that argument.
 		for(DelpArgument arg: args){			
 			DialecticalTree dtree = new DialecticalTree(arg);
+			//Uses a stack-based approach to traverse the dialectical tree and compute defeaters at each node.
 			Deque<DialecticalTree> stack = new ArrayDeque<>();
 			stack.add(dtree);
 			while(!stack.isEmpty()){
@@ -143,6 +200,7 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 				stack.addAll(dtree2.getDefeaters(groundDelp,comparisonCriterion));
 				//inferenceSteps++;
 			}
+			// If the marking of the root (dtree) is UNDEFEATED, sets warrant to true and breaks the loop
 			if(dtree.getMarking().equals(DialecticalTree.Mark.UNDEFEATED)){
 				warrant = true;
 				break;
@@ -150,11 +208,19 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 			 
 		}
 		// get all arguments for ~f (if f is not already warranted)
+		// Initialize comp_warrant: 
+		// Initializes a boolean variable comp_warrant to track whether there is a warrant for the complement of the formula.
 		boolean comp_warrant = false;
+		//Check if warrant is false:
+		//Checks if the boolean variable warrant is false. If warrant is true, the code inside the block is skipped.
 		if(!warrant){		
+			// Retrieves all arguments that support the complement of the formula 
 			args = getArgumentsWithConclusion(groundDelp, (FolFormula) f.complement());
+			// Process Arguments Supporting the Complement Formula:
+			//For each argument (arg) supporting the complement of the formula, creates a dialectical tree (dtree) rooted at that argument.
 			for(DelpArgument arg: args){
 				DialecticalTree dtree = new DialecticalTree(arg);
+				//Uses a stack-based approach to traverse the dialectical tree and compute defeaters at each node.
 				Deque<DialecticalTree> stack = new ArrayDeque<>();
 				stack.add(dtree);
 				while(!stack.isEmpty()){
@@ -162,6 +228,7 @@ public class DelpReasoner implements Reasoner<DelpAnswer.Type,DefeasibleLogicPro
 					stack.addAll(dtree2.getDefeaters(groundDelp,comparisonCriterion));
 					//inferenceSteps++; 
 				}
+				// If the marking of the root (dtree) is UNDEFEATED, sets comp_warrant to true and breaks the loop.
 				if(dtree.getMarking().equals(DialecticalTree.Mark.UNDEFEATED)){
 					comp_warrant = true;
 					break;
